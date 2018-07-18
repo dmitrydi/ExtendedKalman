ec535662a9a996bdfc3cff44553a22eaaa2bde8a
dmitrydi <d.dmitrook@gmail.com>
2018-07-18 10:46:37 +0000

    cleaned

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -129,10 +129,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // Radar updates
     VectorXd z(3);
-    Tools tools;
     z << measurement_pack.raw_measurements_[0], measurement_pack.raw_measurements_[1], measurement_pack.raw_measurements_[3];
-    //Hj_ = tools.CalculateJacobian(ekf_.x_);
-    //ekf_.H_ = Hj_;
     ekf_.R_ = R_radar_;
     ekf_.UpdateEKF(z);
   } else {
